3d9647e1d87ea8936bbe2eb3271da41fdd54eefb
violet978 <villette@live.com>
2022-07-21 19:32:50 -0700

    Added several db queries

--- db/query.sql
+++ db/query.sql
@@ -1,2 +1,5 @@
-
-
+-- Add a department
+-- enter the name of the department and that department is added to the database
+USE employees_db
+INSERT INTO departments (department_name)
+VALUES ("${}");
--- server.js
+++ server.js
@@ -5,12 +5,15 @@ const cTable = require('console.table');
 // Connect to database
 const db = mysql.createConnection(
     {
-      host: 'localhost',
-      user: 'root',
-      password: 'puppy9',
-      database: 'employees_db',
+        host: 'localhost',
+        user: 'root',
+        database: 'employees_db',
+        // PW different on windows
+        password: 'puppy9',
+        // needed for mac only
+        port: '/tmp/mysql.sock',
     },
-  );
+);
 
 const startMenu = () => {
     inquirer
@@ -31,15 +34,56 @@ const startMenu = () => {
             },
         ])
         .then((answers) => {
-            if (answers.initial_selection == "View all employees") {
+            if (answers.initial_selection == "View all departments") {
                 viewDepts();
+            }
+            if (answers.initial_selection == "View all roles") {
+                viewRoles();
+            }
+            if (answers.initial_selection == "View all employees") {
+                viewEmployees();
+            }
+            if (answers.initial_selection == "Add a department") {
+                addDepartment();
+            }
+            if (answers.initial_selection == "Add a role") {
+                addRole();
+            }
+            if (answers.initial_selection == "Add an employee") {
+                addEmployee();
             }
         });
 };
 
 const viewDepts = () => {
     db.query(
-        `SELECT employees.id, employees.first_name, employees.last_name, roles.title, departments.department_name AS department, roles.salary, CONCAT(managers.first_name, ' ',managers.last_name) AS manager
+        `SELECT departments.id, departments.department_name AS department
+        FROM departments
+        ORDER BY departments.id;
+        `,
+        function (err, results) {
+            console.table(results);
+        }
+    );
+};
+
+const viewRoles = () => {
+    db.query(
+    `SELECT roles.id, roles.title AS job_title, departments.department_name AS department, roles.salary
+    FROM roles
+    LEFT JOIN departments
+    ON departments.id = roles.department_id
+    ORDER BY roles.id;
+    `,
+        function (err, results) {
+            console.table(results);
+        }
+    );
+};
+
+const viewEmployees = () => {
+    db.query(
+    `SELECT employees.id, employees.first_name, employees.last_name, roles.title, departments.department_name AS department, roles.salary, CONCAT(managers.first_name, ' ',managers.last_name) AS manager
     FROM departments
     LEFT JOIN roles
     ON departments.id = roles.department_id
@@ -54,5 +98,18 @@ const viewDepts = () => {
         }
     );
 };
+
+const addDepartment = () => {
+    db.query(
+    `USE employees_db
+    INSERT INTO departments (department_name)
+    VALUES ("${}");
+    `,
+        function (err, results) {
+            console.table(results);
+        }
+    );
+};
+
 
 startMenu();
