a99a816a0aedd2346a454c8e1410d217a31a0000
violet978 <villette@live.com>
2022-07-21 17:56:58 -0700

    Added first inquirer prompt

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
 const mysql = require('mysql2');
-const inquirer = require("inquirer");
+const inquirer = require('inquirer');
 const cTable = require('console.table');
 
-
+// Connect to database
 const db = mysql.createConnection(
     {
       host: 'localhost',
       user: 'root',
       password: 'puppy9',
-      database: 'movies_db'
+      database: 'employees_db',
     },
-    console.log(`Connected to the employees_db database.`)
   );
 
-  const startMenu = () => {
+const startMenu = () => {
     inquirer
         .prompt([
             {
                 type: "list",
-                name: 'initial_selection',
+                name: "initial_selection",
                 message: "Choose from the following options:",
                 choices: [
                     "View all departments",
@@ -27,11 +26,33 @@ const db = mysql.createConnection(
                     "Add a department",
                     "Add a role",
                     "Add an employee",
-                    "Update an employee role"
-                ]
-            }
+                    "Update an employee role",
+                ],
+            },
         ])
         .then((answers) => {
-            if (answers.initial_selection == "View all departments") {
-                addManagertoArray();
+            if (answers.initial_selection == "View all employees") {
+                viewDepts();
             }
+        });
+};
+
+const viewDepts = () => {
+    db.query(
+        `SELECT employees.id, employees.first_name, employees.last_name, roles.title, departments.department_name AS department, roles.salary, CONCAT(managers.first_name, ' ',managers.last_name) AS manager
+    FROM departments
+    LEFT JOIN roles
+    ON departments.id = roles.department_id
+    LEFT JOIN employees
+    ON roles.id = employees.role_id
+    LEFT JOIN employees AS managers
+    ON managers.id = employees.manager_id 
+    ORDER BY employees.id;
+    `,
+        function (err, results) {
+            console.table(results);
+        }
+    );
+};
+
+startMenu();
